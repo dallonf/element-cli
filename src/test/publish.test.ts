c9e30ee6f02d3c7b3523e975cda94143011778c3
Dallon Feldner <dallon.feldner@volusion.com>
2020-12-11 15:14:55 -0600

    WIP e2e test

--- src/test/publish.test.ts
+++ src/test/publish.test.ts
@@ -1,6 +1,10 @@
+import { exec as execCb } from "child_process";
 import * as path from "path";
+import { promisify } from "util";
 
-import { copy, mkdirp, pathExists, rmdir } from "fs-extra";
+import { copy, mkdirp, pathExists, remove } from "fs-extra";
+
+const exec = promisify(execCb);
 
 it("publishes (happy path)", async () => {
     const project = path.resolve(".");
@@ -16,11 +20,20 @@ it("publishes (happy path)", async () => {
 
     const tmpDir = path.join(project, ".tmp", testDirName);
     if (await pathExists(tmpDir)) {
-        await rmdir(tmpDir);
+        await remove(tmpDir);
     }
     await mkdirp(tmpDir);
 
     const fixtureDir = path.join(project, "src/test/fixtures/SimpleBlock");
 
     await copy(fixtureDir, tmpDir);
+
+    const elementCli = path.join(project, "bin/src/index.js");
+
+    const command = ["node", elementCli, "publish"].join(" ");
+    const output = await exec(command, {
+        cwd: tmpDir,
+    });
+
+    console.log(command, output);
 });
